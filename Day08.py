790e4035ced790b4a69a74f33a5cf9787c5bf8ce
PippiaEleonora <43842574+PippiaEleonora@users.noreply.github.com>
2023-12-11 23:05:23 +0100

    Update Day08.py

--- Day08.py
+++ Day08.py
@@ -1,28 +1,52 @@
+import math
 
-with open('/uploads/Input08.txt') as f:
-    arrayRaw = f.readlines()
-Graph ={}
-
-arrayRaw = arrayRaw[0].split(')') 
-print(arrayRaw[0][-5:-1])
-for i in range(2,len(arrayRaw)):
-    r = arrayRaw[i]
-    Graph.update({r[1:4]: (r[8:11], r[13:16]) } )
-    
-Node = 'AAA'
-Count = 0
-i = 0
-Instruction = arrayRaw[0].replace('\n','').replace('L','0').replace('R', '1')
-#print(Instruction)
-#print(Graph['AAA'])
-while not Node == 'ZZZ':
-    Node = Graph[Node][int(Instruction[i])]
-    Count += 1
-    i += 1
-    if i == len(Instruction):
-        i = 0
+def mcm_multiplo(Numbers):
+    Curr = Numbers[0]
+    for i in range(1,len(Numbers)):
+        Curr = int((Curr*Numbers[i])/math.gcd(Curr,Numbers[i]))
         
-print(Count)
+    return Curr
 
-#Second star
+if __name__ == '__main__':
+    with open('input08.txt') as f:
+        arrayRaw = f.readlines()
+    Graph ={}
+
+    for i in range(2,len(arrayRaw)):
+        r = arrayRaw[i]
+        Graph.update({r[0:3]: (r[7:10], r[12:15]) } )
+        
+    Node = 'AAA'
+    Count = 0
+    i = 0
+    Instruction = arrayRaw[0].replace('\n','').replace('L','0').replace('R', '1')
+    #print(Instruction)
+    #print(Graph['AAA'])
+    while not Node == 'ZZZ':
+        Node = Graph[Node][int(Instruction[i])]
+        Count += 1
+        i += 1
+        if i == len(Instruction):
+            i = 0
+            
+    print(Count)
+
+    #Second star
+    NodeList = [n for n in list(Graph.keys()) if n[-1] == 'A']
+    EndList = [n for n in list(Graph.keys()) if n[-1] == 'Z']
+
+    CountList = [0]*len(NodeList)
+    for j in range(len(NodeList)):
+        n = NodeList[j]
+        i = 0
+        while not n[-1] == 'Z':
+            n = Graph[n][int(Instruction[i])] 
+            CountList[j] += 1
+            i += 1
+            if i == len(Instruction):
+                i = 0
+        
     
+    print(mcm_multiplo(CountList))
+
+
